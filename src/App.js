7c67f692f9d5f3337332bccec3d88ca38f1b4dd6
bhawna1809 <bhawnatekwani313@gmail.com>
2025-01-18 02:08:46 +0530

    Create Build folder

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Projects from "./components/Projects";
   import Contact from "./components/Contact";
  import Footer from "./components/Footer";
 import Certficates from './components/Certificates';
+import "./output.css"; 
 
 
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,18 +11,18 @@ export default function Contact() {
             
                     <div > 
                     <span className="font-bold">Mobile Number:   </span>
-                    <a href="tel:+918853625286" target="_blank" className="text-blue-800">+91 8853625286
+                    <a href="tel:+918853625286"  rel="noreferrer" target="_blank" className="text-blue-800">+91 8853625286
                      </a> </div>
                
                    <div > 
                     <span className="font-bold">Email address: </span>
-                    <a href="mailto:bhawnatekwani313@gmail.com?subject=Hello&body=Message" target="_blank" className="text-blue-800">
+                    <a href="mailto:bhawnatekwani313@gmail.com?subject=Hello&body=Message"  rel="noreferrer" target="_blank" className="text-blue-800">
                     bhawnatekwani313@gmail.com </a></div> 
 
                     <div> 
                     <span className="font-bold">LinkedIn: </span>
 
-                    <a href="https://www.linkedin.com/in/bhawna-tekwani" target="_blank" className="text-blue-800">Bhawna Tekwani</a>
+                    <a href="https://www.linkedin.com/in/bhawna-tekwani" target="_blank" rel="noreferrer"  className="text-blue-800">Bhawna Tekwani</a>
                     </div> 
                     
             </div>
